ab934453e9b328722c5a1a982db86f8b8f860fd2
Anastasia Kalinina <ask@d-element.ru>
2019-07-22 10:07:42 +0500

    начальная сборка стилей

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 'use strict';
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const debug = require('gulp-debug');
+const sourcemaps = require('gulp-sourcemaps');
+const gulpIf = require('gulp-if');
+const del = require('del');
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
-gulp.task('hello', function(callback) {
-  console.log("Hello!");
-  callback();
+gulp.task('styles', function() {
+  return gulp.src('src/styles/styles.scss')
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write('.')))
+    .pipe(gulp.dest('public'));
 });
+
+gulp.task('clean', function () {
+  return (async () => {
+    const deletedPaths = await del(['public/**']);
+  })();
+});
+
+gulp.task('assets', function() {
+  return gulp.src('src/assets/**')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', gulp.series('clean', gulp.parallel('assets','styles')));
